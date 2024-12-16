4beed3962471acd31539c2a60a9de9bd3431719e
Samira <samarraazizi@gmail.com>
2024-12-16 19:45:04 +0500

    ai_agent_data

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.query_engine import PandasQueryEngine
+
+
+
+load_dotenv()
+
+population_path = os.path.join("data", "population.csv")
+population_df = pd.read_csv(population_path)
+
+print(population_df.head())
